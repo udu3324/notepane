46a8146c8572851b00caa68ce8fda8c14456abca
udu3324 <47045986+udu3324@users.noreply.github.com>
2026-01-31 02:24:43 -0600

    add docker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
-<h1>notepane</h1>
+<script>
+	import { goto } from "$app/navigation";
+	import { resolve } from "$app/paths";
+
+</script>
+
+<div>
+    <h1>notepane</h1>
+</div>
+
+<button on:click={() => goto(resolve("/login"))}>login</button>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { goto } from "$app/navigation";
+	import { resolve } from "$app/paths";
+
+    let password
+
+    function login() {
+        console.log("logging in")
+    }
+</script>
+<div>
+    login
+    <form on:submit={login}>
+        <input bind:value={password} on:submit={login()} placeholder="password" type="text" class="border-2 border-solid">
+    </form>
+    
+    <button on:click={() => goto(resolve("/"))}>back</button>
+</div>
